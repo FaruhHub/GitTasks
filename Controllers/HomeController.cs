2746de6eef592ad633def3284443f257d19e5ffc
Faruh Jumabayev <faruh_jumabayev@mail.ru>
2015-07-06 02:47:26 +0300

    211: создание класса HomeController
    содержит в себе логику работы web-приложения

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HotelService.Models;
+
+namespace HotelService.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            ViewBag.Rooms = RoomRepository.GetRooms();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registration(RegistrationForm model)
+        {
+            return View("Success", model);
+        }
+
+    }
+}
