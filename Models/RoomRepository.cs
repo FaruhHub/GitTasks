02fea4a4d41ddc00a030a23e9dfaf7c548fd4457
Faruh Jumabayev <faruh_jumabayev@mail.ru>
2015-07-06 02:44:47 +0300

    112: добавление класса RoomRepository
    содержит список репозиторий отеля

---
+++ Models/RoomRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotelService.Models
+{
+    public static class RoomRepository
+    {
+        public static List<Room> GetRooms()
+        {
+            return new List<Room>
+                       {
+                           new Room { Name = "Номер с видом на океан"},
+                           new Room { Name = "Одиночный номер"},
+                           new Room { Name = "Номер с бассейном и кинотеатром"}
+                       };
+        }
+    }
+}
