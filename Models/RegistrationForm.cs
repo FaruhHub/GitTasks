31d379610b10117b62f1588f5cab59ead51531fe
Faruh Jumabayev <faruh_jumabayev@mail.ru>
2015-07-06 03:00:25 +0300

    213: добавление атрибутов в модели RegistrationForm (валидация данных)
    для проверки полей вводимых пользователем

--- Models/RegistrationForm.cs
+++ Models/RegistrationForm.cs
@@ -7,10 +7,20 @@ namespace HotelService.Models
 {
     public class RegistrationForm
     {
+        [Required(ErrorMessage = "Введите ваше имя")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Введите вашу фамилию")]
         public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Введите ваше отчество")]
         public string MiddleName { get; set; }
+
+        [Required(ErrorMessage = "Введите ваш телефон")]
+        [RegularExpression(@"\d+", ErrorMessage = "Вы ввели некорректный номер телефона")]
         public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Укажите комнату")]
         public Room SelectedRoom { get; set; }
     }
 }
